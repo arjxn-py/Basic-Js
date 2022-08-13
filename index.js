f6b7e1f5182d7630e8bd29f1271dd4b6f2828d65
Arjun.py <104268427+arjxn-py@users.noreply.github.com>
2022-08-13 18:39:56 +0530

    Some Basic Operations

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello How are you?")
+// console.log("Hello I am Learning Javascript.")
+
+// var myName = 'Arjun';
+// var myAge = 19;
+// var my_name = 'arjun';
+// var _last__name = 'Verma';
+
+// console.log(myName);
+// console.log(myAge);
+// console.log(my_name);
+// console.log(_last__name);
+
+// console.log(typeof(myAge));
+
+console.log(10 + "20");
+console.log(9 - "5");
+console.log("Java" + "Script");
